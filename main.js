05f5adfb25e635fb42275d191a47762bed370f4d
amirkhezri <85443248+amirkhezri@users.noreply.github.com>
2026-04-30 23:15:49 +0330

    Update main.js

--- main.js
+++ main.js
@@ -83,6 +83,7 @@ window.addEventListener('load', () => {
         userUsername  = user.username   || 'None';
 
         const initials = (userFirstName[0] + (userLastName ? userLastName[0] : '')).toUpperCase().trim();
+        avatarInitials.textContent = initials || '?';
 
     }
 
@@ -456,7 +457,6 @@ window.startSubscriptionListener = async function () {
 
         const avatarImg = document.getElementById('user-avatar-img');
         const avatarInitials = document.getElementById('user-avatar-initials');
-        avatarInitials.textContent = initials || '?';
           if (data.photo_url && avatarImg) {
                     if (avatarImg.src !== data.photo_url) {
                     avatarImg.src = data.photo_url + '?t=' + Data.now();
